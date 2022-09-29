8b79b70f594b90cfc737b9506695528ecae4f713
Alberto Polanco <prmus10@gmail.com>
2022-09-29 11:50:35 -0500

    Functional TDD

--- inclass/src/1_5_tdd/functional/bowling.spec.ts
+++ inclass/src/1_5_tdd/functional/bowling.spec.ts
@@ -1,6 +1,65 @@
+import {createGame, Game} from "./bowling"
 
 describe('A bowling game', () => {
-    
+
+    let game: Game
+
+    beforeEach(() => {
+        game = createGame()
+    })
+
+    // beforeEach handles the initialization of game.
+    /*it('should invoke create Game', function () {
+        game = createGame();
+    });*/
+
+    it('should roll a ball', function () {
+        game = game.roll(0);
+    });
+
+    //Fill the game with a horrible game where all the rolls are 0
+    it('should do a gutter game', function () {
+        game = rollMany(game, 20, 0);
+        expect(game.score()).toBe(0)
+    });
+
+    it('should make a game where all rolls are ones.', function () {
+        game = rollMany(game, 20, 1)
+        expect(game.score()).toBe(20)
+    });
+
+    it('should make a spare', function () {
+        game = rollSpare(game);
+        game = rollMany(game, 17, 0);
+        expect(game.score()).toBe(20);
+    });
+
+    it('should make a strike', function () {
+        game = rollStrike(game);
+        game = rollMany(game, 16, 0);
+        expect(game.score()).toBe(14);
+    });
 
 });
+
+function rollMany(game: Game, rolls: number, scoreInRolls: number) {
+    for (let pins = 0; pins < rolls; pins++) {
+        game = game.roll(scoreInRolls);
+    }
+    return game;
+}
+
+function rollSpare(game: Game) {
+    game = game.roll(5);
+    game = game.roll(5);
+    game = game.roll(5);
+    return game;
+}
+
+function rollStrike(game: Game) {
+    game = game.roll(10);
+    game = game.roll(1);
+    game = game.roll(1);
+    return game;
+}
 
--- inclass/src/1_5_tdd/functional/bowling.ts
+++ inclass/src/1_5_tdd/functional/bowling.ts
@@ -1,1 +1,59 @@
 //the code
+
+type State = { rolls: number[] }
+
+const initialState = {rolls: []}
+
+export const createGame = (state: State = initialState) => Object.freeze({
+    roll: (pinsKnockedDown: number) => roll(state, pinsKnockedDown),
+    score: () => score(state)
+});
+
+const roll = (state: State, pinsKnockedDown: number) => {
+    const rolls = [...state.rolls, pinsKnockedDown]
+    console.log(rolls)
+    return createGame({rolls});
+};
+
+const score = ({rolls}: State) => {
+    let score = 0;
+    let firstInFrame = 0;
+
+    for (let frame = 0; frame < 10; frame++) {
+        if (isStrike(rolls, firstInFrame)) {
+            score += 10 + nextTwoBallsForStrike(rolls, firstInFrame)
+            firstInFrame++;
+        }else if (isSpare(rolls, firstInFrame)) { //Spare
+            score += 10 + nextBallForSpare(rolls, firstInFrame)
+            firstInFrame += 2;
+        } else {
+            score += ballsInFrame(rolls, firstInFrame)
+            firstInFrame += 2;
+        }
+
+    }
+
+    return score;
+};
+
+const isSpare = (rolls: number[], rollIndex: number) => {
+    return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+}
+
+function isStrike(rolls: number[], firstInFrame: number) {
+    return rolls[firstInFrame] == 10;
+}
+
+function nextTwoBallsForStrike(rolls: number[], firstInFrame: number) {
+    return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+}
+
+function nextBallForSpare(rolls: number[], firstInFrame: number) {
+    return rolls[firstInFrame + 2];
+}
+
+function ballsInFrame(rolls: number[], firstInFrame: number) {
+    return rolls[firstInFrame] + rolls[firstInFrame + 1];
+}
+
+export type Game = ReturnType<typeof createGame>
